eb0dbe0428bafc80aafa7c679090e8c79caad081
Boris Khomut <boris.khomut@gmail.com>
2015-07-25 22:46:59 -0400

    some profile styling

--- Emelem/ProfileTableViewController.swift
+++ Emelem/ProfileTableViewController.swift
@@ -30,7 +30,7 @@ class ProfileTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 2
+        return 5
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,7 +39,7 @@ class ProfileTableViewController: UITableViewController {
         if section == 0 {
             return 1
         } else {
-            return 4
+            return 1
         }
     }
 
@@ -48,10 +48,37 @@ class ProfileTableViewController: UITableViewController {
 
         if indexPath.section == 0 {
             let cell1: ProfileCell = tableView.dequeueReusableCellWithIdentifier("ProfileCell", forIndexPath: indexPath) as! ProfileCell
+                currentUser()?.getPhoto({
+                    image in
+                    cell1.profileImageView.layer.masksToBounds = true
+                    cell1.profileImageView.contentMode = .ScaleAspectFill
+                    cell1.profileImageView.image = image
+                    })
             return cell1
-        } else {
+        } else if indexPath.section == 1 {
             let cell2: SettingCell = tableView.dequeueReusableCellWithIdentifier("SettingCell", forIndexPath: indexPath) as! SettingCell
+            cell2.settingButton.setTitle("Commissions", forState: .Normal)
+            cell2.contentView.backgroundColor = UIColor.redColor()
+            /*cell2.settingButton.setImage(<#image: UIImage?#>, forState: .Normal) */
             return cell2
+        } else if indexPath.section == 2 {
+            let cell3: SettingCell = tableView.dequeueReusableCellWithIdentifier("SettingCell", forIndexPath: indexPath) as! SettingCell
+            cell3.settingButton.setTitle("Credit Card", forState: .Normal)
+            cell3.contentView.backgroundColor = UIColor.blueColor()
+            /*cell3.settingButton.setImage(<#image: UIImage?#>, forState: .Normal) */
+            return cell3
+        } else if indexPath.section == 3 {
+            let cell4: SettingCell = tableView.dequeueReusableCellWithIdentifier("SettingCell", forIndexPath: indexPath) as! SettingCell
+            cell4.settingButton.setTitle("Address", forState: .Normal)
+            cell4.contentView.backgroundColor = UIColor.greenColor()
+            /*cell4.settingButton.setImage(<#image: UIImage?#>, forState: .Normal) */
+            return cell4
+        } else {
+            let cell5: SettingCell = tableView.dequeueReusableCellWithIdentifier("SettingCell", forIndexPath: indexPath) as! SettingCell
+            cell5.settingButton.setTitle("About Shwapp", forState: .Normal)
+            cell5.contentView.backgroundColor = UIColor.yellowColor()
+            /*cell5.settingButton.setImage(<#image: UIImage?#>, forState: .Normal) */
+            return cell5
         }
     }
     
